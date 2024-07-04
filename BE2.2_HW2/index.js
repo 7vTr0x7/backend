8f7d575fe9eb3bcaf9862e87685f043f88972e44
7vTr0x7 <7vTrox7@gmail.com>
2024-07-04 16:27:54 +0530

    BE2.2_HW2 - read + create

--- BE2.2_HW2/index.js
+++ BE2.2_HW2/index.js
@@ -4,26 +4,56 @@ const Hotels = require("./models/hotels.models");
 initializeDatabase();
 
 const newHotel = {
-  name: "New Hotel",
+  name: "Lake View",
   category: "Mid-Range",
-  location: "123 Main Street, Frazer Town",
-  rating: 4.0,
+  location: "124 Main Street, Anytown",
+  rating: 3.2,
   reviews: [],
-  website: "https://hotel-example.com",
-  phoneNumber: "+1234567890",
+  website: "https://lake-view-example.com",
+  phoneNumber: "+1234555890",
   checkInTime: "2:00 PM",
   checkOutTime: "12:00 PM",
-  amenities: ["Laundry", "Room Service"],
+  amenities: ["Laundry", "Boating"],
   priceRange: "$$$ (31-60)",
   reservationsNeeded: true,
-  isParkingAvailable: true,
+  isParkingAvailable: false,
   isWifiAvailable: true,
   isPoolAvailable: false,
   isSpaAvailable: false,
+  isRestaurantAvailable: false,
+  photos: [
+    "https://example.com/hotel1-photo1.jpg",
+    "https://example.com/hotel1-photo2.jpg",
+  ],
+};
+
+const newHotel1 = {
+  name: "Sunset Resort",
+  category: "Resort",
+  location: "12 Main Road, Anytown",
+  rating: 4.0,
+  reviews: [],
+  website: "https://sunset-example.com",
+  phoneNumber: "+1299655890",
+  checkInTime: "2:00 PM",
+  checkOutTime: "11:00 AM",
+  amenities: [
+    "Room Service",
+    "Horse riding",
+    "Boating",
+    "Kids Play Area",
+    "Bar",
+  ],
+  priceRange: "$$$$ (61+)",
+  reservationsNeeded: true,
+  isParkingAvailable: true,
+  isWifiAvailable: true,
+  isPoolAvailable: true,
+  isSpaAvailable: true,
   isRestaurantAvailable: true,
   photos: [
-    "https://example.com/hotel-photo1.jpg",
-    "https://example.com/hotel-photo2.jpg",
+    "https://example.com/hotel2-photo1.jpg",
+    "https://example.com/hotel2-photo2.jpg",
   ],
 };
 
@@ -31,10 +61,44 @@ const createHotel = async (newHotel) => {
   try {
     const hotel = new Hotels(newHotel);
     const savedHotel = await hotel.save();
-    console.log("Hotel:", savedHotel);
+    console.log("Saved Hotel:", savedHotel);
   } catch (error) {
     throw error;
   }
 };
 
-createHotel(newHotel);
+// createHotel(newHotel);
+// createHotel(newHotel1);
+
+const readAllHotels = async () => {
+  try {
+    const allHotels = await Hotels.find();
+    console.log("All Hotels:", allHotels);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// readAllHotels();
+
+const readHotelByName = async (hotelName) => {
+  try {
+    const hotelByName = await Hotels.findOne({ name: hotelName });
+    console.log("Hotel By Name:", hotelByName);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// readHotelByName("Lake View");
+
+const readAllHotelsWithParking = async () => {
+  try {
+    const hotelsWithParking = await Hotels.find({ isParkingAvailable: true });
+    console.log("Hotels With Parking:", hotelsWithParking);
+  } catch (error) {
+    throw error;
+  }
+};
+
+readAllHotelsWithParking();
