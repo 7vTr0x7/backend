7a4f4a4d4112fa3a459b3270afe3ca62ad8e4f27
7vTr0x7 <7vTrox7@gmail.com>
2024-07-05 10:46:53 +0530

    BE2_Assignment

--- BE2_Assignment/index.js
+++ BE2_Assignment/index.js
@@ -3,37 +3,3 @@ const fs = require("fs");
 const Cars = require("./models/cars.models");
 
 initializeDatabase();
-
-const jsonData = fs.readFileSync("BE1_Assignment/cars.json");
-
-const carsData = JSON.parse(jsonData);
-
-const seedData = () => {
-  try {
-    for (const car of carsData) {
-      const newCar = new Cars({
-        brand: car.brand,
-        model: car.model,
-        year: car.year,
-        bodyStyle: car.bodyStyle,
-        fuelType: car.fuelType,
-        transmission: car.transmission,
-        engine: car.engine,
-        mileage: car.mileage,
-        color: car.color,
-        price: car.price,
-        condition: car.condition,
-        description: car.description,
-        photos: car.photos,
-        inMarket: car.isMarket,
-      });
-
-      newCar.save();
-      console.log("Car Data:", newCar.brand);
-    }
-  } catch (error) {
-    console.log("Error seeding data", error);
-  }
-};
-
-seedData();
