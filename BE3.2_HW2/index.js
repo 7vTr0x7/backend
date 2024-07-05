db37c6ca1832b7e234bd4c54a0facd48d0d6eb94
7vTr0x7 <7vTrox7@gmail.com>
2024-07-05 15:24:56 +0530

    BE3.2_HW2 - movies get & post

--- BE3.2_HW2/index.js
+++ BE3.2_HW2/index.js
@@ -1,9 +1,38 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Express server.");
+});
+
+const movies = [
+  { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010 },
+
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: 1972,
+  },
+];
+
+app.post("/movies", (req, res) => {
+  const newMovie = req.body;
+
+  if (newMovie.title && newMovie.director && newMovie.year) {
+    movies.push(newMovie);
+    res
+      .status(201)
+      .json({ message: "Movie added successfully", movie: newMovie });
+  } else {
+    res.status(400).json({ error: "Title, Director and Year are required" });
+  }
+});
+
+app.get("/movies", (req, res) => {
+  res.send(movies);
 });
 
 const PORT = 4000;
