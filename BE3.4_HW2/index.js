cdafa1ded30a3587ff97fdc4ca82533d3d137fbe
7vTr0x7 <7vTrox7@gmail.com>
2024-07-05 19:05:51 +0530

    BE3.4_HW2 -  update & get items

--- BE3.4_HW2/index.js
+++ BE3.4_HW2/index.js
@@ -34,18 +34,52 @@ app.post("/movies/:id", (req, res) => {
   const movieId = Number(req.params.id);
   const dataToUpdate = req.body;
 
-  const movieToUpdate = movies.find((movie) => movie.id === movieId);
+  const itemToUpdate = movies.find((movie) => movie.id === movieId);
 
-  if (!movieToUpdate) {
+  if (!itemToUpdate) {
     res.status(404).json({ error: "Movie not Found" });
   } else {
     if (dataToUpdate.title && dataToUpdate.director && dataToUpdate.year) {
-      Object.assign(movieToUpdate, dataToUpdate);
+      Object.assign(itemToUpdate, dataToUpdate);
       res
         .status(200)
-        .json({ message: "movie updated successfully", movie: movieToUpdate });
+        .json({ message: "movie updated successfully", movie: itemToUpdate });
     } else {
       res.status(400).json({ error: "title, director and year are required" });
+    }
+  }
+});
+
+const items = [
+  { id: 1, itemName: "Spoon", color: "Silver", quantity: 8 },
+
+  { id: 2, itemName: "Fork", color: "Silver", quantity: 8 },
+
+  { id: 3, itemName: "Plate", color: "Off-White", quantity: 6 },
+];
+
+app.get("/", (req, res) => {
+  res.send(items);
+});
+
+app.post("/items/:id", (req, res) => {
+  const itemId = Number(req.params.id);
+  const updatedItemData = req.body;
+
+  const itemToUpdate = items.find((item) => item.id === itemId);
+
+  if (!itemToUpdate) {
+    res.status(404).json({ error: "Movie not Found" });
+  } else {
+    if (
+      updatedItemData.itemName &&
+      updatedItemData.color &&
+      updatedItemData.quantity
+    ) {
+      Object.assign(itemToUpdate, updatedItemData);
+      res.status(200).json({ message: "Item updated successfully" });
+    } else {
+      res.status(400).json({ error: "itemName, color and " });
     }
   }
 });
