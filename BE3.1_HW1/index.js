1b571abb0e74e4d1dae21cf5978c30067e79ff93
7vTr0x7 <7vTrox7@gmail.com>
2024-07-05 12:07:21 +0530

    BE3.1_HW1

--- BE3.1_HW1/index.js
+++ BE3.1_HW1/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express");
+  res.send("Hello, Express JS");
 });
 
 app.get("/about", (req, res) => {
