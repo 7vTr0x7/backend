52ad8f725cd64ffe28101181644fbadd2f80e205
7vTr0x7 <7vTrox7@gmail.com>
2024-07-05 12:19:22 +0530

    BE3.1_HW2

--- BE3.1_HW2/index.js
+++ BE3.1_HW2/index.js
@@ -6,6 +6,20 @@ app.get("/", (req, res) => {
   res.send("Hello from express server.");
 });
 
+app.get("/signin", (req, res) => {
+  res.send("This is the Sign In page.");
+});
+
+app.get("/booking", (req, res) => {
+  res.send("Book your tickets here.");
+});
+app.get("/clothing/kids", (req, res) => {
+  res.send("This is the kids wear page.");
+});
+app.get("/blog", (req, res) => {
+  res.send("This is the blog page.");
+});
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server Running on port: ${PORT}`);
