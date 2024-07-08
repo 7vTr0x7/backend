23670bbfe13243c4a2065d525db541318d1b016f
7vTr0x7 <7vTrox7@gmail.com>
2024-07-08 15:38:40 +0530

    BE4.1_HW2 - express

--- BE4.1_HW2/index.js
+++ BE4.1_HW2/index.js
@@ -1,74 +1,12 @@
+const express = require("express");
 const { initializeDatabase } = require("./db/db.connect");
 const Hotels = require("./models/hotels.models");
 
+const app = express();
+
+app.use(express.json());
+
 initializeDatabase();
-
-const newHotel = {
-  name: "Lake View",
-  category: "Mid-Range",
-  location: "124 Main Street, Anytown",
-  rating: 3.2,
-  reviews: [],
-  website: "https://lake-view-example.com",
-  phoneNumber: "+1234555890",
-  checkInTime: "2:00 PM",
-  checkOutTime: "12:00 PM",
-  amenities: ["Laundry", "Boating"],
-  priceRange: "$$$ (31-60)",
-  reservationsNeeded: true,
-  isParkingAvailable: false,
-  isWifiAvailable: true,
-  isPoolAvailable: false,
-  isSpaAvailable: false,
-  isRestaurantAvailable: false,
-  photos: [
-    "https://example.com/hotel1-photo1.jpg",
-    "https://example.com/hotel1-photo2.jpg",
-  ],
-};
-
-const newHotel1 = {
-  name: "Sunset Resort",
-  category: "Resort",
-  location: "12 Main Road, Anytown",
-  rating: 4.0,
-  reviews: [],
-  website: "https://sunset-example.com",
-  phoneNumber: "+1299655890",
-  checkInTime: "2:00 PM",
-  checkOutTime: "11:00 AM",
-  amenities: [
-    "Room Service",
-    "Horse riding",
-    "Boating",
-    "Kids Play Area",
-    "Bar",
-  ],
-  priceRange: "$$$$ (61+)",
-  reservationsNeeded: true,
-  isParkingAvailable: true,
-  isWifiAvailable: true,
-  isPoolAvailable: true,
-  isSpaAvailable: true,
-  isRestaurantAvailable: true,
-  photos: [
-    "https://example.com/hotel2-photo1.jpg",
-    "https://example.com/hotel2-photo2.jpg",
-  ],
-};
-
-const createHotel = async (newHotel) => {
-  try {
-    const hotel = new Hotels(newHotel);
-    const savedHotel = await hotel.save();
-    console.log("Saved Hotel:", savedHotel);
-  } catch (error) {
-    throw error;
-  }
-};
-
-// createHotel(newHotel);
-// createHotel(newHotel1);
 
 const readAllHotels = async () => {
   try {
@@ -78,8 +16,6 @@ const readAllHotels = async () => {
     throw error;
   }
 };
-
-// readAllHotels();
 
 const readHotelByName = async (hotelName) => {
   try {
