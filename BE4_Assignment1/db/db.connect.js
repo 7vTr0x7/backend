a63819b2b9cca1a269a7f10a2ddf0683f840bae9
7vTr0x7 <7vTrox7@gmail.com>
2024-07-09 16:24:29 +0530

    BE4_Assignment1 - db

---
+++ BE4_Assignment1/db/db.connect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const mongoURI =
+  process.env.MONGODB ||
+  "mongodb+srv://neoGStudent:vTroxGAMING@neog.vf9x0bo.mongodb.net/?retryWrites=true&w=majority&appName=neoG";
+
+const initializeDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(mongoURI);
+    if (connection) {
+      console.log("Connected to MongoDB");
+    }
+  } catch (error) {
+    throw error;
+    console.log("Failed to Connect MongoDB");
+  }
+};
+
+module.exports = { initializeDatabase };
